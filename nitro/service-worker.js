6dfba666c86c814d45f48c514f314b2b3bcde3f5
Nguyen Quoc Tham <tham.nguyenquoc@gameloft.com>
2019-11-22 17:03:13 +0700

    - update

--- nitro/service-worker.js
+++ nitro/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d15cea74d7275ff49946e2ebd1ddeee3.js"
+  "/nitro/precache-manifest.248c78695f1dbd0c68c6bed57f856f7c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nitro/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
